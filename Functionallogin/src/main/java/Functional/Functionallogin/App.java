0c09f864a1bfc271ef11911f5ed4ffc5d03d3c12
Ravitharadha <radhakrishnanravitha@gmail.com>
2025-07-31 11:59:47 +0530

    initial commit

---
+++ Functionallogin/src/main/java/Functional/Functionallogin/App.java
@@ -0,0 +1,13 @@
+package Functional.Functionallogin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Functionallogin/src/test/java/Functional/Functionallogin/AppTest.java
@@ -0,0 +1,50 @@
+package Functional.functionallogin;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement; // FIXED: Correct import
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager; // FIXED: Typo in class name
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class LoginTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup(); // No need to manually set path
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+
+    @Test(priority = 1)
+    public void testValidLogin() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.name("password")).sendKeys("admin123"); // FIXED: typo 'by' and 'sendkeys'
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        Thread.sleep(3000);
+        WebElement dashboard = driver.findElement(By.xpath("//h6[text()='Dashboard']"));
+        Assert.assertTrue(dashboard.isDisplayed(), "Login Failed - Dashboard not found");
+    }
+
+    @Test(priority = 2)
+    public void testInvalidLogin() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.findElement(By.name("username")).sendKeys("invalidUser");
+        driver.findElement(By.name("password")).sendKeys("wrongPass");
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        Thread.sleep(3000);
+        WebElement errorMsg = driver.findElement(By.xpath("//p[contains(text(),'Invalid credentials')]"));
+        Assert.assertTrue(errorMsg.isDisplayed(), "Error message not shown for invalid login");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
